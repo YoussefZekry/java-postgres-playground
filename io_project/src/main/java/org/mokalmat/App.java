6bc0412e7edf1e4224d403329e56f955c45e6472
YoussefZekry <youssefzekry97@gmail.com>
2023-06-22 19:06:15 +0200

    Read data from csv file one by one

--- io_project/src/main/java/org/mokalmat/App.java
+++ io_project/src/main/java/org/mokalmat/App.java
@@ -1,9 +1,14 @@
 package org.mokalmat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Scanner;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
@@ -19,44 +24,28 @@ public class App {
         dataSource.setUser("postgres");
         dataSource.setPassword("2111976");
 
+        String csvFilePath = "D:\\playground\\myCSV.csv";
+        File csv = new File(csvFilePath);
+
         Connection connection = null;
         // using datasource object to get a connection to the database
         try {
-            connection = dataSource.getConnection();
-            System.out.println("my message -> connected to database successfully.");
-        } catch (SQLException e) {
+            Scanner myReader = new Scanner(csv);
+            myReader.nextLine();
+            while (myReader.hasNext()) {
+                String[] data = myReader.nextLine().split(",");
+                for ( String value : data) {
+                    System.out.println(value + " ");
+                }
+            }
+        } catch (Exception e) {
             System.out.println("my message -> failed to connect to database: "
                     + e.getMessage());
         }
 
-        try {
-            //calling add order 
-            // CallableStatement stm1 = connection.prepareCall("{ ? = call add_ordersAdv(?) }");
-            // stm1.registerOutParameter(1, Types.INTEGER);
-            // stm1.setString(2, "D:\\playground\\myCSV.csv");
-            // stm1.execute();
-            // int result1 = stm1.getInt(1);
-            // System.out.println("add_orders result: " + result1);
-
-            //calling delete order
-            // CallableStatement stm2 = connection.prepareCall("{ ? = call delete_orderAdv(?) }");
-            // stm2.registerOutParameter(1, Types.VARCHAR);
-            // stm2.setInt(2, 56);
-            // stm2.execute();
-            // String result2 = stm2.getString(1);
-            // System.out.println("delete_orderAdv result: " + result2);
-
-            //calling update order
-            CallableStatement stm3 = connection.prepareCall("{ ? = call update_orderAdv(?,?) }");
-            stm3.registerOutParameter(1, Types.VARCHAR);
-            stm3.setInt(2,-5);
-            stm3.setString(3, "new status");
-            stm3.execute();
-             String result = stm3.getString(1);
-            System.out.println("update_orderAdv result: " + result);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        
 
     }
 }
+   // String sql = "insert into orders (customer_id,order_date,amount,status) values (?,?,?,?)";
+            // PreparedStatement statement = connection.prepareStatement(sql);
