d60c5e8bd42782aaf0e481c93ed57114c74322c2
YoussefZekry <youssefzekry97@gmail.com>
2023-06-25 16:22:57 +0300

    Add "reduce" method

--- JsMethods.js
+++ JsMethods.js
@@ -1,14 +1,14 @@
 let items = [
-	{name: 'Bike', price: 100},
-	{name: 'TV', price: 200},
-	{name: 'Album', price: 10},
-	{name: 'Book', price: 50},
-	{name: 'Phone', price: 500},
-	{name: 'Computer', price: 800},
-	{name: 'Keyboard', price: 120},
+  { name: "Bike", price: 100 },
+  { name: "TV", price: 200 },
+  { name: "Album", price: 10 },
+  { name: "Book", price: 50 },
+  { name: "Phone", price: 500 },
+  { name: "Computer", price: 800 },
+  { name: "Keyboard", price: 120 },
 ];
 
-let newItems = items.every((item)=> {
-	return item.price <= 500;
-});
-console.log(newItems);
+let total = items.reduce((currentTotal, item) => {
+  return item.price + currentTotal;
+},0);
+console.log(total);
